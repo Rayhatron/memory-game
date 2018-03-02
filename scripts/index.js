8cd4fa1d4fc94adda05cbe02c21439742262239b
Rufaro Madamombe <rayhatron@gmail.com>
2018-03-02 10:30:44 +0200

    feat: Match Cards
    Implemented logic and data to check if cards match and if they do they stay flipped
    
    Otherwise they both get hidden the player can select again
    
    Once the player has matched 8 pairs then we congratulate them, not fully implemented yet
    
    Resolves: #6

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,9 @@ const cards = [...cardsNodelist];
 
 let clickedCardsNumber = 0; // Track number of clicked cards
 let clickedCards = []; // Track the cards that have been clicked
+let matchedCards = 0; // Track the number of cards that have been matched to determine if winner or not
 
-function displayClickedCard () {
+function displayClickedCard() {
     // Limit the number of cards that can be revealed to 2
     if(clickedCardsNumber < 2) {
         console.log("Clicked");
@@ -19,7 +20,43 @@ function displayClickedCard () {
         this.classList.toggle("disable-card-click");
         console.log(clickedCards);
     }
+
+    if(clickedCardsNumber == 2){
+        // Once we have selected 2 cards, let's see if they match or not
+        matchClickedCards();
+    }
     
+}
+
+function matchClickedCards() {
+    // Check if the icon is the same and if it is then they match and allow cards to be selected
+    if(clickedCards[0].dataset.icon == clickedCards[1].dataset.icon){
+        console.log("Match!");
+        matchedCards++;
+        // Once we have 8 matches it means the player has won the game so let's congratulate them
+        if(matchedCards == 8){
+            // TODO: Make a fancy congratulations message :) and ask the user if they want to play again 
+            // As well as show them their Star rating and time it took to complete.
+            alert("Winner!");
+        }else{
+            allowCardsToBeSelected();
+        }
+    }else{
+        // Wait a second before hiding the cards since they didn't match and allow more cards to be selected
+        setTimeout(() => {
+            clickedCards.forEach((card) => {
+                card.classList.remove("show-card");
+                card.classList.remove("disable-card-click");
+                allowCardsToBeSelected();
+            });
+        }, 1000);
+    }
+
+}
+
+function allowCardsToBeSelected() {
+    clickedCardsNumber = 0;
+    clickedCards = [];
 }
 
 function shuffleArray(array) {
